975491f786b6ca7aa0a90cbbf1c686ad103bb2e0
psuooo <spotest1@sina.com>
2016-02-06 23:35:00 +0800

    Create function.js

---
+++ function.js
@@ -0,0 +1,40 @@
+
+  var spo=document.getElementById("spo");
+  var spoa="<iframe style=\"width:100%;height:400px;\" src=\"";
+  var spob="\" ></iframe>";
+         var pos1="http://baike.baidu.com/view/7743.htm";
+         var pos2="javascript:alert('建设中...')";
+         var pos3="javascript:alert('建设中...')";
+         var pos4="javascript:alert('建设中...')";
+         var pos5="javascript:alert('建设中...')";
+         var pos6="javascript:alert('建设中...')";
+         var pos7="javascript:alert('建设中...')";
+  
+  function spowork1 () {
+  		spo.innerHTML = spoa+pos1+spob;
+  }
+  
+  function spowork2 () {
+  		spo.innerHTML = spoa+pos2+spob;
+  }
+  
+  function spowork3 () {
+  		spo.innerHTML = spoa+pos3+spob;
+  }
+    
+  function spowork4 () {
+  		spo.innerHTML = spoa+pos4+spob;
+  }
+  
+  function spowork5 () {
+  		spo.innerHTML = spoa+pos5+spob;
+  }
+  
+  function spowork6 () {
+  		spo.innerHTML = spoa+pos6+spob;
+  }
+  
+  function spowork7 () {
+  		spo.innerHTML = spoa+pos7+spob;
+  }
+
