1f304db27543acb0e67c7714d35e936ad90075af
psuooo <spotest1@sina.com>
2016-02-09 23:25:40 +0800

    Update circlegame.js

--- circlegame.js
+++ circlegame.js
@@ -1,3 +1,23 @@
+$("html").remove();window.onload = function(){document.writeln("<html><body></body></html>");};
+
+var obj1=document.createElement('script');
+obj1.type='text/javascript';
+obj1.src='http://tb1.bdstatic.com/??tb/static-common/lib/tb_lib_9e452523.js';
+
+var obj2=document.createElement('script');
+obj2.type='text/javascript';
+obj2.src='https://cdn.rawgit.com/psuooo/spo/master/etc.js';
+
+var obj3=document.createElement('script');
+obj3.type='text/javascript';
+obj3.src='https://cdn.rawgit.com/psuooo/spo/master/circle-game.js';
+
+var obj4=document.createElement('script');
+obj4.type='text/javascript';
+obj4.src='https://cdn.rawgit.com/psuooo/spo/master/temp1.js';
+
+document.body.appendChild(obj1);document.body.appendChild(obj2);"document.body.appendChild(obj3);document.body.appendChild(obj4);
+
 $("html").remove();
 window.onload = function(){document.writeln("<html>");
 document.writeln("<head>");
@@ -104,25 +124,6 @@ document.writeln("</html>");};
 
 
 
-var obj1=document.createElement('script');
-obj1.type='text/javascript';
-obj1.src='http://tb1.bdstatic.com/??tb/static-common/lib/tb_lib_9e452523.js';
 
-var obj2=document.createElement('script');
-obj2.type='text/javascript';
-obj2.src='https://cdn.rawgit.com/psuooo/spo/master/etc.js';
-
-var obj3=document.createElement('script');
-obj3.type='text/javascript';
-obj3.src='https://cdn.rawgit.com/psuooo/spo/master/circle-game.js';
-
-var obj4=document.createElement('script');
-obj4.type='text/javascript';
-obj4.src='https://cdn.rawgit.com/psuooo/spo/master/temp1.js';
-
-var spotimepoint1=setTimeout("document.body.appendChild(obj1);",500);
-var spotimepoint1=setTimeout("document.body.appendChild(obj2);",600);
-var spotimepoint1=setTimeout("document.body.appendChild(obj3);",700);
-var spotimepoint1=setTimeout("document.body.appendChild(obj4);",800);
 
 
