57cfede807fed5740de0f339b91210118bfb7e4e
psuooo <spotest1@sina.com>
2016-02-22 17:08:56 +0800

    Update function.js

--- function.js
+++ function.js
@@ -9,6 +9,7 @@
          var pos5="http://xgz.bcschool.cn:8080/czbg_gaoer/uploadimg/QRcode.png";
          var pos6="http://xgz.bcschool.cn:8080/czbg_gaoer/studentpage.php?studentid=Circle";
          var pos7="javascript:alert('建设中...');parent.spowork0 ()";
+         var pos8="";
   
   function spowork0 () {
             var spo=document.getElementById("spo");
@@ -41,5 +42,15 @@
   
   function spowork7 () {
   		spo.innerHTML = spoa+pos7+spob;
+  }
+  function spowork8 () {
+  		spo.innerHTML = '<li><a target="_blank" href="http://shang.qq.com/wpa/qunwpa?idkey=2d5b0b27bcb4f1fdcabccce6bc5871e56cf7995224501dbc1d72dad78a7ed059">加入我们的性趣群</a></li>\
+  		<li><a href="tencent://Message/?Uin=1564872310&amp;Menu=yes" target="_blank">与教育大师讨论</a></li>\
+  		<li><a href="http://www.amazon.cn/s/url=search&amp;field-keywords=%E6%80%A7%E6%95%99%E8%82%B2" target="_blank">教育书籍</a></li>\
+  		<li><a href="http://group.mtime.com/filmov/discussion/676501/" target="_blank">教育电影</a></li>\
+  		<li><a href="https://www.baidu.com/s?wd=%E7%9C%9F%E4%BA%BA%E6%80%A7%E6%95%99%E8%82%B2%E7%9F%AD%E7%89%87" target="_blank">真人短片</a></li>\
+  		<li><a href="http://v.baidu.com/comic/9975.htm?fr=ala8&amp;ty=43" target="_blank">健康儿童</a></li>\
+  		<li><a href="http://baike.baidu.com/link?url=VKKVZEpselVTtOcqetIuesTehbIUIn0U3Dcviq_lHJIDyCZuEYORtotlcTJhTAH7YPtfPs_EpCwnXeui6w6FIK" target="_blank">权威知识</a></li>\
+  		<li><a href="javascript:alert(\'建设中...\')" target="_blank">其他相关</a></li>';
   }
 
